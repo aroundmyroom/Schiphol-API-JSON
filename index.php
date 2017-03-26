c32d336a408486bc3f9a04b1eef8e48c1c11ff10
aroundmyroom <dennis@aroundmyroom.com>
2017-03-26 12:38:17 +0200

    updates met vertrek informatie
    2e api call geïmplementeerd die nav eerder verkregen destinationcode de
    code invult zodat je land en plaats krijgt van aankomende vlucht tevens
    status aangepast van de landingen (EXP, LND, AIR etc.. )

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ EOT;
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "$base_url/public-flights/flights?app_id=$app_id&app_key=$app_key&scheduletime=$scheduletime&flightdirection=A&includedelays=false&page=0&sort=%2Bscheduletime");
+curl_setopt($ch, CURLOPT_URL, "$base_url/public-flights/flights?app_id=$app_id&app_key=$app_key&scheduletime=$scheduletime&flightdirection=A&includedelays=true&page=0&sort=%2Bscheduletime");
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, -1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
@@ -69,29 +69,104 @@ foreach ($json['flights'] as $flight)
         echo "Bagageband: {$belt} <br />";
     }
 
-    foreach ($flight['route']['destinations'] as $departure)
-    {
-       echo "Vertrek luchthaven: {$departure} <br />";
-    }
-
     foreach ($flight['codeshares']['codeshares'] as $joinedwith)
     {
-      echo "Ook bekend onder Vluchtnummer: {$joinedwith} <br />";
+     echo "Ook bekend onder Vluchtnummer: {$joinedwith} <br />";
     }
 
-  
+    foreach ($flight['route']['destinations'] as $departure)
+    {
+
+$ch2 = curl_init();
+
+curl_setopt($ch2, CURLOPT_URL, "$base_url/public-flights/destinations/$departure?app_id=$app_id&app_key=$app_key");
+
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, -1);
+curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "GET");
+
+$headers = array();
+$headers[] = "Accept: application/json";
+$headers[] = "Resourceversion: v1";
+curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers);
+
+$results2 = curl_exec($ch2);
+curl_close($ch2);
+
+if (substr($results2, 0, 3) == "\xEF\xBB\xBF") {
+  $results = substr($results2, 3);
+}
+
+$json2 = json_decode($results2, true);
+
+// foreach ($json2{'city'} as $city)
+
+$city = ($json2{'city'});
+$country = ($json2{'country'});
+$nlcity = ($json2['publicName']{'dutch'});
+
+       echo "Vertrokken van luchthaven: $nlcity <br />";
+       echo "Vertrokken uit: $country <br />";
+       echo "City code: $departure <br />";
+
+//       echo "Vertrek luchthaven: {$departure} <br />";
+
+    }
+
+
     $status_new = $flight['publicFlightState']['flightStates'][0];
     $status_old = $flight['publicFlightState']['flightStates'][1];
 
-	   echo "Status van de vlucht is: $status_new <br />";
-	   echo "Status van de vlucht was: $status_old <br />";
+if (empty($status_new)) {
+    echo "Geen vluchtstatus momenteel";
+}
 
 
-       echo "<br />";
+ if(!empty($status_new)){
+     switch($status_new) {
+       case "": 
+        Echo "Geen geldige waarde gevonden";
+       break;
 
+      case "SCH":
+         echo "Vlucht volgens Schema ($status_new) <br />";
+           echo "Status van de vlucht was: $status_old <br /><br />";  
+       break;
+
+      case "LND":
+         echo "Vlucht is geland <br /><br />";
+       break;
+
+      case "FIR":
+        echo "Vlucht is in nederlands luchtruim <br /> <br />";
+       break;
+
+      case "AIR":
+         echo "Vlucht is nog onderweg <br /><br />";
+       break;
+
+       case "CNX":
+         echo "Vlucht is gecancelled <br /><br />";     
+        break;
+  
+      default: 
+         echo "Hier mag wat staan als default<br /> <br />";
+}
+ 
+    }
+else{
+
+    echo "bladiebla";
+    echo "<br />";
 
 }
 
+}
+
+//	   echo "Status van de vlucht is: $status_new <br />";
+//	   echo "Status van de vlucht was: $status_old <br />";
+
+
+       echo "<br />";
  echo "<br />";
 
 ?>
